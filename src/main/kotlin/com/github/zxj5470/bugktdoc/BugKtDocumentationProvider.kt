e56355807dc9537331db442e942d80254b239a5f
2938137849 <2938137849@qq.com>
2022-07-24 15:33:26 +0800

    暂存

--- src/main/kotlin/com/github/zxj5470/bugktdoc/BugKtDocumentationProvider.kt
+++ src/main/kotlin/com/github/zxj5470/bugktdoc/BugKtDocumentationProvider.kt
@@ -9,9 +9,7 @@ import com.intellij.lang.documentation.CodeDocumentationProvider
 import com.intellij.lang.documentation.DocumentationProviderEx
 import com.intellij.lang.java.JavaDocumentationProvider.getPackageInfoComment
 import com.intellij.openapi.util.Pair
-import com.intellij.psi.PsiComment
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiField
+import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.containers.isNullOrEmpty
 import org.jetbrains.kotlin.kdoc.psi.impl.KDocImpl
@@ -28,33 +26,41 @@ class BugKtDocumentationProvider : DocumentationProviderEx(), CodeDocumentationP
 	 * @param startPoint PsiElement
 	 * @return Pair<PsiElement, PsiComment>?
 	 */
-	override fun parseContext(startPoint: PsiElement): Pair<PsiElement, PsiComment>? {
+	override fun parseContext(startPoint: PsiElement): Pair<PsiElement, PsiComment> {
 		var current = startPoint
 		while (true) {
-			if (current is KDocImpl) {
-				return Pair.create(if (current is PsiField) current.modifierList else current, current)
-			} else if (PackageUtil.isPackageInfoFile(current)) {
-				return Pair.create(current, getPackageInfoComment(current))
-			}
+			if (current is KDocImpl) return Pair.create(current, current)
+			else if (PackageUtil.isPackageInfoFile(current)) return Pair.create(current, getPackageInfoComment(current))
 			current = current.parent
 		}
 	}
 
 	private fun PsiComment.getOwner() = PsiTreeUtil.getParentOfType<KtDeclaration>(this)
-		?: this.parent.takeIf { it is KtFunction || it is KtClass || it is KtSecondaryConstructor }
+		?: this.parent.takeIf { it is KtFunction || it is KtClass }
 
-	override fun generateDocumentationContentStub(contextComment: PsiComment?): String? {
+	override fun generateDocumentationContentStub(contextComment: PsiComment?): String {
 		if (!pluginActive || contextComment == null) return ""
-		val owner = contextComment.getOwner()
-		val commenter = LanguageCommenters.INSTANCE.forLanguage(contextComment.language) as CodeDocumentationAwareCommenter
 
-		fun StringBuilder.appendDecorate(str: String) = append(CodeDocumentationUtil.createDocCommentLine(str, contextComment.containingFile, commenter))
+		return when (val owner = contextComment.getOwner()) {
+			is KtNamedFunction -> docKtNamedFunction(owner, contextComment)
+			is KtClass -> docKtClass(owner, contextComment)
+			is KtConstructor<*> -> docKtConstructor(owner, contextComment)
+			else -> ""
+		}
+	}
 
-		fun docKtNamedFunction(owner: KtNamedFunction) = buildString {
+	override fun findExistingDocComment(contextElement: PsiComment?): PsiComment? =
+		if (!isKotlinNative) (contextElement as? KDocImpl)?.getOwner()?.docComment else contextElement
+
+	private fun docKtNamedFunction(owner: KtNamedFunction, contextComment: PsiComment): String {
+		val pair: Pair<PsiFile, CodeDocumentationAwareCommenter> = contextComment.pair()
+		return buildString {
 			// @receiver
 			owner.receiverTypeReference?.let {
-				appendDecorate(RECEIVER)
+				append(RECEIVER, pair)
+				append("[")
 				append(it.text)
+				append("]")
 				append(LF)
 			}
 
@@ -62,22 +68,27 @@ class BugKtDocumentationProvider : DocumentationProviderEx(), CodeDocumentationP
 			owner.valueParameters.forEach {
 				val param = it.nameIdentifier?.text ?: ""
 				val type = it.itsType
-				appendDecorate(PARAM)
+				append(PARAM, pair)
 				// add a space before `param` and after is no used
-				append("$param $type")
+				append("$param [$type]")
 				append(LF)
 			}
 
 			// @return
 			if (owner.hasDeclaredReturnType()) {
-				appendDecorate(RETURN)
+				append(RETURN, pair)
+				append("[")
 				append(owner.typeReference?.typeElement?.text)
+				append("]")
 				append(LF)
-			} else {
+			}
+			else {
 				owner.itsType.let {
 					if (isAlwaysShowUnitReturnType || it != "Unit") {
-						appendDecorate(RETURN)
+						append(RETURN, pair)
+						append("[")
 						append(it)
+						append("]")
 						append(LF)
 					}
 				}
@@ -87,94 +98,95 @@ class BugKtDocumentationProvider : DocumentationProviderEx(), CodeDocumentationP
 			PsiTreeUtil.findChildrenOfType(owner, KtAnnotationEntry::class.java)
 				.firstOrNull { it.calleeExpression?.text == "Throws" }
 				?.valueArguments?.forEach {
-				(it.getArgumentExpression() as? KtClassLiteralExpression)?.let {
-					PsiTreeUtil.findChildOfType(it, KtNameReferenceExpression::class.java)?.text?.let {
-						appendDecorate(THROWS)
-						append(it)
-						append(LF)
-					}
-				}
-			}
-		}
-
-		fun docKtClass(owner: KtClass): String {
-			return buildString {
-				owner.typeParameters.forEach {
-					appendDecorate(PARAM)
-					append(it.text)
-					append(LF)
-				}
-
-				// order: 1. primary Parameters -> @property
-				owner.primaryConstructorParameters.forEach {
-					// is property
-					if (it.hasValOrVar()) {
-						val param = it.nameIdentifier?.text
-						val type = it.itsType
-						if (!param.isNullOrEmpty() && !type.isEmpty()) {
-							appendDecorate(PROPERTY)
-							// add a space before or after is no used
-							append("$param $type")
+					(it.getArgumentExpression() as? KtClassLiteralExpression)?.let {
+						PsiTreeUtil.findChildOfType(it, KtNameReferenceExpression::class.java)?.text?.let {
+							append(THROWS, pair)
+							append("[")
+							append(it)
+							append("]")
 							append(LF)
 						}
 					}
 				}
-
-				// order: 2. class fields -> @property
-				if (isAlwaysShowClassFieldProperty)
-					owner.getProperties().forEach {
-						val param = it.nameIdentifier?.text
-						val type = it.itsType
-						if (!param.isNullOrEmpty()) {
-							appendDecorate(PROPERTY)
-							// add a space before or after is no used
-							append("$param $type")
-							append(LF)
-						}
-					}
-
-				// @constructor
-				if (owner.hasPrimaryConstructor() && isAlwaysShowConstructor) {
-					// empty class
-					if (!owner.getPrimaryConstructorParameterList()?.parameters.isNullOrEmpty()) {
-						appendDecorate(CONSTRUCTOR)
-						append(LF)
-					}
-				}
-			}
-		}
-
-		fun docKtConstructor(owner: KtConstructor<*>): String {
-			return buildString {
-
-				// @param
-				owner.getValueParameters().forEach {
-					val param = it.nameIdentifier?.text
-					val type = it.itsType
-					if (!param.isNullOrEmpty() && !type.isEmpty()) {
-						appendDecorate(PARAM)
-						append("$param $type")
-						append(LF)
-					}
-				}
-
-				// @constructor
-				if (isAlwaysShowConstructor) {
-					appendDecorate(CONSTRUCTOR)
-					append(LF)
-				}
-			}
-		}
-
-		return when (owner) {
-			is KtNamedFunction -> docKtNamedFunction(owner)
-			is KtClass -> docKtClass(owner)
-			is KtConstructor<*> -> docKtConstructor(owner)
-			else -> ""
 		}
 	}
 
+	private fun docKtClass(owner: KtClass, contextComment: PsiComment): String {
+		val pair: Pair<PsiFile, CodeDocumentationAwareCommenter> = contextComment.pair()
+		return buildString {
+			owner.typeParameters.forEach {
+				append(PARAM, pair)
+				append("[")
+				append(it.text)
+				append("]")
+				append(LF)
+			}
 
-	override fun findExistingDocComment(contextElement: PsiComment?): PsiComment? =
-		if (!isKotlinNative) (contextElement as? KDocImpl)?.getOwner()?.docComment else contextElement
+			// order: 1. primary Parameters -> @property
+			owner.primaryConstructorParameters.forEach {
+				// is property
+				if (it.hasValOrVar()) {
+					val param = it.nameIdentifier?.text
+					val type = it.itsType
+					if (!param.isNullOrEmpty() && type.isNotEmpty()) {
+						append(PROPERTY, pair)
+						// add a space before or after is no used
+						append("$param [$type]")
+						append(LF)
+					}
+				}
+			}
+
+			// order: 2. class fields -> @property
+			if (isAlwaysShowClassFieldProperty)
+				owner.getProperties().forEach {
+					val param = it.nameIdentifier?.text
+					val type = it.itsType
+					if (!param.isNullOrEmpty()) {
+						append(PROPERTY, pair)
+						// add a space before or after is no used
+						append("$param [$type]")
+						append(LF)
+					}
+				}
+
+			// @constructor
+			if (owner.hasPrimaryConstructor() && isAlwaysShowConstructor) {
+				// empty class
+				if (!owner.getPrimaryConstructorParameterList()?.parameters.isNullOrEmpty()) {
+					append(CONSTRUCTOR, pair)
+					append(LF)
+				}
+			}
+		}
+	}
+
+	private fun docKtConstructor(owner: KtConstructor<*>, contextComment: PsiComment): String {
+		val pair: Pair<PsiFile, CodeDocumentationAwareCommenter> = contextComment.pair()
+		return buildString {
+
+			// @param
+			owner.getValueParameters().forEach {
+				val param = it.nameIdentifier?.text
+				val type = it.itsType
+				if (!param.isNullOrEmpty() && type.isNotEmpty()) {
+					append(PARAM, pair)
+					append("$param [$type]")
+					append(LF)
+				}
+			}
+
+			// @constructor
+			if (isAlwaysShowConstructor) {
+				append(CONSTRUCTOR, pair)
+				append(LF)
+			}
+		}
+	}
+
+	private fun PsiComment.pair() =
+		Pair(containingFile, LanguageCommenters.INSTANCE.forLanguage(language) as CodeDocumentationAwareCommenter)
+
+	private fun StringBuilder.append(str: String, arg: Pair<PsiFile, CodeDocumentationAwareCommenter>) =
+		append(CodeDocumentationUtil.createDocCommentLine(str, arg.first, arg.second))
 }
